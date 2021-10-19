440916055ff5309db9ea201b2fd1ac763aecfa3f
WSAIOUDI <wassim.saioudi@ynov.com>
2021-10-19 20:29:25 +0200

    home modifié

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,35 @@
 <template>
-  <hello-world />
+<main>
+  <button @click="open=!open">Nouveau message</button>
+  <popup v-if="open">
+    <hello-world />
+    <button @click="open=!open" class="popup-close">
+      close
+    </button>
+  </popup>
+</main>
 </template>
 
 <script lang="ts">
+import Popup from '@/components/Popup.vue'
 import Vue from 'vue'
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from '../components/SendMessage.vue'
 
 export default Vue.extend({
   name: 'Home',
 
   components: {
-    HelloWorld
+    HelloWorld,
+    Popup
+  },
+  data () {
+    return {
+      open: false
+    }
   }
 })
 </script>
+
+<style>
+
+</style>
