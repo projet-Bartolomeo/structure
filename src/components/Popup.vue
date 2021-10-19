137e5c08f650902504c65f2cd796cfcd46b13d6b
WSAIOUDI <wassim.saioudi@ynov.com>
2021-10-19 20:26:39 +0200

    gestionPopup

---
+++ src/components/Popup.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="popup">
+        <div class="popup-inner">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.popup {
+    position: fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    z-index: 99;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(107, 103, 103,0.5);
+
+    .popup-inner {
+        background-color: blanchedalmond;
+        padding-left: 200px;
+        padding-right: 200px;
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+}
+</style>
